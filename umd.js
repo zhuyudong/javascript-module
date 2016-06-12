607a6c67e6a81c6f654dac70154b38c4c2b96ac6
jedi_knight <zhuyudong@aliyun.com>
2016-06-12 09:29:49 +0800

    注释

--- umd.js
+++ umd.js
@@ -1,7 +1,8 @@
 (function(window, factory) {
+  // 先判断是否支持Node.js的模块
   if (typeof exports === 'object') {
     module.exports = factory();
-  } else if (typeof define === 'function' && define.amd) {
+  } else if (typeof define === 'function' && define.amd) { // 再判断是否支持AMD
     define(factory);
   } else {
     window.eventUtil = factory();
